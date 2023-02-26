762c228e3137ff5a90bddc364e0f2c52aedbaed1
Dorian Voland <dorian.voland@gmail.com>
2023-02-26 19:55:32 +0100

    aa

--- scripte.js
+++ scripte.js
@@ -1,4 +1,4 @@
-var CheckedButtonColor = "blue"
+var path = "https://volanddo.github.io/"
 
 function animateMenu(){
     let menuToggle = document.querySelector("header .toggle");
@@ -64,9 +64,94 @@ function changeLanguage(){
     }
 }
 
+class Article{
+    title;
+    presentation;
+    competence;
+    image;
+    externButton;
+    constructor(title, presentation, competence, image, externButton){
+        this.title = title;
+        this.presentation = presentation;
+        this.image = image;
+        this.competence = competence;
+        this.externButton = externButton;
+    }
+
+    createArticle(){
+        let article = document.createElement("article");
+        article.classList.add("article");
+        let div = document.createElement("div");
+        let h2 = document.createElement("h2");
+        let ul = document.createElement("ul");
+        // generation title
+        div.classList.add("title");
+        article.appendChild(div);
+        h2.innerHTML = this.title["h2"];
+        div.appendChild(h2);
+        div.appendChild(ul);
+        this.title["ul"].forEach(liText => {
+            let li = document.createElement("li");
+            li.innerHTML = liText;
+            ul.appendChild(li);
+        });
+        // Presentation
+        this.presentation.forEach(textPres =>{
+            let p = document.createElement("p");
+            p.classList.add("presentation")
+            p.innerHTML = textPres;
+            article.appendChild(p);
+        });
+        //externButton
+        //<p class="externButton fr"> <a href="">Fichier source non disponible (accord avec l'ENSIMAG)</a></p>
+        let p = document.createElement("p");
+        article.appendChild(p);
+        p.classList.add("externButton");
+        let a = document.createElement("a");
+        p.appendChild(a);
+        a.href = this.externButton["link"];
+        a.innerHTML = this.externButton["text"];
+        //image
+        div = document.createElement("div");
+        div.classList.add("image");
+        article.appendChild(div);
+        this.image.forEach(imagePath =>{
+            let img = document.createElement("img");
+            img.src = imagePath;
+            div.appendChild(img);
+        });
+        //competence
+        div = document.createElement("div");
+        div.classList.add("competence");
+        article.appendChild(div);
+        h2 = document.createElement("h2");
+        h2.innerHTML = this.competence["title"];
+        div.appendChild(h2);
+        ul = document.createElement("ul");
+        div.appendChild(ul);
+        this.competence["ul"].forEach(liText => {
+            let li = document.createElement("li");
+            li.innerHTML = liText;
+            ul.appendChild(li);
+        });
+        return article;
+    }
+}
+
+function generateProject(){
+    fetch(path+'project.json')
+  .then(response => response.json())
+  .then(data => {
+    
+    let a = new Article(data.cv-fr.title, data.cv-fr.presentation, data.cv-fr.competence, data.cv-fr.image, data.cv-fr.externButton);
+    document.getElementById("MyProject").appendChild(a.createArticle());
+  })
+  .catch(error => console.error(error));
+}
 
 
 window.addEventListener('load', function() {
     animateMenu();
-    changeLanguage();
+    //changeLanguage();
+    generateProject();
 });
